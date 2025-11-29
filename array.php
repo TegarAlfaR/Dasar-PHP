db8d549b80b4c3358844f8dde2f69f4e5143ed2d
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-11-29 20:25:11 +0700

    function and array

---
+++ array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $a = ["apel", "jeruk", "mangga", "pisang"];
+
+    $b = ['indonesia' => 'jakarta', 'inggris' => 'london', 'jerman' => 'berlin'];
+
+    echo $a[1];
+    echo "<br>";
+    echo $b['inggris'];
+    ?>
+</body>
+
+</html>
